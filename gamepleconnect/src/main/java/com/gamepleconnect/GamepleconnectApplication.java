8e58e00f2376d756d58a8e654985dd43f82bfaa8
KimSeungJin <dahyun101107@naver.com>
2023-06-29 15:36:52 +0900

    feat : 프로젝트 최초 세팅
    프로젝트 최초 세팅 및 DB 연결

---
+++ gamepleconnect/src/main/java/com/gamepleconnect/GamepleconnectApplication.java
@@ -0,0 +1,13 @@
+package com.gamepleconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamepleconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamepleconnectApplication.class, args);
+	}
+
+}
