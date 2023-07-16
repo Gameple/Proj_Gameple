1fc62889672d8a3c6099a23af1796043c9069a7e
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2023-07-16 18:55:05 +0900

    feat : Game Entity 설계

---
+++ gamepleconnect/src/main/java/com/gamepleconnect/game/entity/Game.java
@@ -0,0 +1,30 @@
+package com.gamepleconnect.game.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Getter
+@Entity
+@Table(name = "gameple_game")
+@NoArgsConstructor
+public class Game {
+
+    @Id
+    @Column(name = "game_code")
+    private Long gameCode;
+
+    @Column(name = "game_alias", nullable = false)
+    private String gameAlias;
+
+    @Builder
+    public Game(Long gameCode, String gameAlias) {
+        this.gameCode = gameCode;
+        this.gameAlias = gameAlias;
+    }
+}
