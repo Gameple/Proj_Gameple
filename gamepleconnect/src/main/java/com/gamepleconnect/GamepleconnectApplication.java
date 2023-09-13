75973930e249f898f62d9fc146073704bdd3ad76
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2023-09-14 00:10:19 +0900

    refactor : 브랜치 API 캐시 적용

--- gamepleconnect/src/main/java/com/gamepleconnect/GamepleconnectApplication.java
+++ gamepleconnect/src/main/java/com/gamepleconnect/GamepleconnectApplication.java
@@ -3,10 +3,12 @@ package com.gamepleconnect;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableCaching
 public class GamepleconnectApplication {
 
 	public static void main(String[] args) {
--- gamepleconnect/src/main/java/com/gamepleconnect/branch/dto/response/IpGeolocationApiResponse.java
+++ gamepleconnect/src/main/java/com/gamepleconnect/branch/dto/response/IpGeolocationApiResponse.java
@@ -3,9 +3,11 @@ package com.gamepleconnect.branch.dto.response;
 import lombok.Builder;
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
 @Builder
-public class IpGeolocationApiResponse {
+public class IpGeolocationApiResponse implements Serializable {
 
     private String ip;
 
