8c6aa7cdd43fdb36a4a6e0bab9a373e426e5e165
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2023-09-15 00:06:12 +0900

    refactor : Ehcache 관련 수정

--- gamepleconnect/src/test/java/com/gamepleconnect/branch/controller/BranchControllerTest.java
+++ gamepleconnect/src/test/java/com/gamepleconnect/branch/controller/BranchControllerTest.java
@@ -6,6 +6,7 @@ import com.gamepleconnect.promotion.reservation.dto.ReservationRequestDto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -19,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@AutoConfigureCache
 @AutoConfigureMockMvc
 @SpringBootTest
 class BranchControllerTest {
--- gamepleconnect/src/test/java/com/gamepleconnect/promotion/reservation/controller/ReservationControllerTest.java
+++ gamepleconnect/src/test/java/com/gamepleconnect/promotion/reservation/controller/ReservationControllerTest.java
@@ -12,6 +12,7 @@ import com.gamepleconnect.root.language.domain.Language;
 import com.gamepleconnect.root.language.repository.LanguageRepository;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -28,6 +29,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+@AutoConfigureCache
 @AutoConfigureMockMvc
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
