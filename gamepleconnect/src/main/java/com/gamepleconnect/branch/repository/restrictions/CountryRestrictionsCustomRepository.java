68b3a3420f855c692b082dcfaf1a6c3e5b54a72b
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2025-08-12 20:32:51 +0900

    feat : 지역 제한 정보 조회 API 구현
    1차에서 미비했던 게임코드, 언어코드별 조회 조건 추가 및 요청값 대소문자 정규화

--- gamepleconnect/src/main/java/com/gamepleconnect/branch/repository/restrictions/CountryRestrictionsCustomRepository.java
+++ gamepleconnect/src/main/java/com/gamepleconnect/branch/repository/restrictions/CountryRestrictionsCustomRepository.java
@@ -1,10 +1,11 @@
 package com.gamepleconnect.branch.repository.restrictions;
 
 import com.gamepleconnect.branch.domain.CountryRestrictions;
+import com.gamepleconnect.branch.dto.request.CountryRestrictionsRequest;
 
 import java.util.List;
 
 public interface CountryRestrictionsCustomRepository {
 
-    List<CountryRestrictions> findByCountryCode(String countryCode);
+    List<CountryRestrictions> findByCountryCodeAndLanguageCode(CountryRestrictionsRequest request);
 }
