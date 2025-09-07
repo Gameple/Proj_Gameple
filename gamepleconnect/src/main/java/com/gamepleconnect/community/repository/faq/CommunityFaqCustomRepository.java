7369c8626aa2817d009e2792bfa19c2c3b3ff35d
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2025-09-07 15:20:47 +0900

    feat : 커뮤니티 FAQ 조회 API 페이징 기능 추가

--- gamepleconnect/src/main/java/com/gamepleconnect/community/repository/faq/CommunityFaqCustomRepository.java
+++ gamepleconnect/src/main/java/com/gamepleconnect/community/repository/faq/CommunityFaqCustomRepository.java
@@ -1,11 +1,9 @@
 package com.gamepleconnect.community.repository.faq;
 
 import com.gamepleconnect.community.dto.request.CommunityFaqRequest;
-import com.gamepleconnect.community.dto.response.CommunityFaqResponse;
-
-import java.util.List;
+import com.gamepleconnect.community.dto.response.CommunityFaqPageResponse;
 
 public interface CommunityFaqCustomRepository {
 
-    List<CommunityFaqResponse> findByGameCodeAndLanguageCode(CommunityFaqRequest request);
+    CommunityFaqPageResponse findByGameCodeAndLanguageCode(CommunityFaqRequest request);
 }
